6c82e6e5e9a7e1dfa6e8cb7afefea18129cc6914
Gabriel Bomtempo <60982898+g-bomtempo@users.noreply.github.com>
2022-11-09 16:21:49 -0300

    Create index.php
    Criação do arquivo principal

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Controle de Estoque</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="container">
+        <h1>Controle de Estoque</h1>        
+            <h2>Produto 1</h2>
+                <p>Folhas A4</p>
+                    <button>Adicionar</button>
+                    <button>Remover</button>  
+    </div>
+</body>
+
+</html>
