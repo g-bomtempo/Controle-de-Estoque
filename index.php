ec36c2df9b9e789c82cf35f662ecfcd25ad15770
Gabriel Bomtempo <60982898+g-bomtempo@users.noreply.github.com>
2022-11-10 17:46:36 -0300

    Update index.php
    Pequenas alterações no HTML e no arquivo

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once "config.php";
 include_once "produtos.php";
 
-$materiais = new Produtos;
+$materiais = new produtos($mysql);
 $dados = $materiais->exibirTodos();
 
 ?>
@@ -19,12 +20,14 @@ $dados = $materiais->exibirTodos();
 <body>
     <div id="container">
         <h1>Controle de Estoque</h1>        
-            <h2><?php foreach ($dados as $materiais):?></h2>
-                <p><?php echo $materiais['produto']; ?></p>
+            <h2><?php foreach ($dados as $materiais):?>
+                <a href="detalhado.php?id=<?php echo $materiais['id']; ?>"><?php echo $materiais['produto']; ?></a>
+            </h2>
+            
                     <p class="quantidade"><?php echo $materiais['quantidade'];?></p>
                     <button>Adicionar</button>
                     <button>Remover</button> 
-
+                    <hr size="1">
         <?php endforeach;?>                 
     </div>
 </body>
