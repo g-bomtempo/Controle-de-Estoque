b53e8398ab531f88d1a757e2b68922d30aa31b02
Gabriel Bomtempo <60982898+g-bomtempo@users.noreply.github.com>
2022-11-10 17:49:26 -0300

    Update produtos.php
    Adição da função de encontrarPorId que faz a conexao com o MySql

--- produtos.php
+++ produtos.php
@@ -2,21 +2,28 @@
 
 class Produtos{
 
+    private $mysql;
+
+    public function __construct(mysqli $mysql)
+    {
+        $this->mysql = $mysql;
+    }
+
     public function exibirTodos():array{
 
-        $produtos =[
-            [
-            'produto' => 'Folha A4',
-            'quantidade' => '5',
-            ],
-            [
-            'produto' => 'Folha A5',
-            'quantidade' => '6',
-            ]
-            
-        ];
+        $resultado = $this->mysql->query('SELECT id, produto, quantidade FROM controledosestoques');
+        $produtos = $resultado->fetch_all(MYSQLI_ASSOC);
 
         return $produtos;
 
     }
+
+    public function encontrarPorId(string $id): array{
+        $selecionaProduto = $this->mysql->prepare("SELECT id, produto, quantidade FROM controleDosEstoques WHERE id = ?");
+        $selecionaProduto->bind_param('s', $id);  
+        $selecionaProduto->execute();
+        $artigo = $selecionaProduto->get_result()->fetch_assoc();  
+        return $artigo;
+    }
+
 }
